879f73973b7f0d6057f2b9a551e4779d5c3b0168
ali <85955355+Alidl81@users.noreply.github.com>
2025-01-25 01:25:13 +0330

    Update server.py

--- server.py
+++ server.py
@@ -160,4 +160,4 @@ def handle_typing(data):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=8080, debug=True)
